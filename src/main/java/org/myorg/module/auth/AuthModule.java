8d52addb4df4ed711a26e6514c6ce6f4b67da072
ansimonof <ansimonof@yandex.ru>
2022-02-12 22:39:52 +0300

    refactoring

--- src/main/java/org/myorg/module/auth/AuthModule.java
+++ src/main/java/org/myorg/module/auth/AuthModule.java
@@ -3,11 +3,15 @@ package org.myorg.module.auth;
 import org.myorg.modules.modules.Module;
 import org.myorg.modules.modules.BootModule;
 import org.myorg.modules.modules.exception.ModuleException;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @BootModule(
         uuid = AuthModuleConsts.UUID
+)
+@PropertySource(
+        value = "boot-properties/auth.application.properties"
 )
 public class AuthModule extends Module {
 
